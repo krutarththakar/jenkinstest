73f58efcfae8f18f9b4ec729fd1354c081df0157
krutarththakar <krutarth.thakar8@gmail.com>
2021-08-31 16:52:15 +0530

    newupadate

--- Hi.java
+++ Hi.java
@@ -3,6 +3,6 @@ class Hi
      public static void main(String[] args)
      {
         System.out.println("Hello");
-        System.out.println("Hello,Hi Dhruv hear");
+        System.out.println("Hello,Hi Krutarth hear");
      }
 }
